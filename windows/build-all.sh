c272f63de57420746673b9655c68d6bf443b1683
Carlos Lozano Diez <carlos@adaptive.me>
2018-08-18 22:31:29 +0200

    Build all windows boxes.

---
+++ windows/build-all.sh
@@ -0,0 +1,5 @@
+packer build -parallel=false windows_2016.json
+packer build -parallel=false windows_2012_r2.json
+packer build -parallel=false windows_2012.json
+packer build -parallel=false windows_2008_r2.json
+packer build -parallel=false windows_10.json
