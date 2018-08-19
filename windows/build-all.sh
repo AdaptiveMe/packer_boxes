169cf0b8f5e17c93f42f6800799ff53542eb202a
Carlos Lozano Diez <carlos@adaptive.me>
2018-08-19 17:58:48 +0200

    Retire 2012 build -> R2 instead.

--- windows/build-all.sh
+++ windows/build-all.sh
@@ -1,5 +1,4 @@
 packer build -only=virtualbox-iso windows_2016.json
 packer build -only=virtualbox-iso windows_2012_r2.json
-packer build -only=virtualbox-iso windows_2012.json
 packer build -only=virtualbox-iso windows_2008_r2.json
 packer build -only=virtualbox-iso windows_10.json
