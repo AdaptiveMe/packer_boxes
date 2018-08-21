740c351030793b6c9ad4688d7b445a6bb279bdcd
Carlos Lozano Diez <carlos@adaptive.me>
2018-08-21 13:08:16 +0200

    Build all for all provisioners.

--- windows/build-all.sh
+++ windows/build-all.sh
@@ -1,4 +1,5 @@
-packer build -only=virtualbox-iso windows_2016.json
-packer build -only=virtualbox-iso windows_2012_r2.json
-packer build -only=virtualbox-iso windows_2008_r2.json
-packer build -only=virtualbox-iso windows_10.json
+packer build windows_10.json
+packer build windows_2016.json
+packer build windows_2012_r2.json
+packer build windows_2008_r2.json
+
